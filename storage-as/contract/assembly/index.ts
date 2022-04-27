ea23aa2fc6be3e8895225ae120166421b130ef58
gagdiez <gagdiez@r2.home>
2022-04-27 18:25:35 +0200

    Reorganized for docs

--- storage-as/contract/assembly/index.ts
+++ storage-as/contract/assembly/index.ts
@@ -1,9 +1,16 @@
 import { storage, u128 } from "near-sdk-as";
+import { PersistentMap, PersistentVector, PersistentSet, AVLTree } from "near-sdk-as";
 
 // Constants
 export const ONE_NEAR: u128 = u128.from("1000000000000000000000000")
 
-// Storage setters
+// Collections
+export let vector = new PersistentVector<i32>("unique-id-vector1");
+export let map = new PersistentMap<string, i32>("unique-id-map1");
+export let set = new PersistentSet<i32>("unique-id-set1");
+export let tree = new AVLTree<string, i32>("unique-id-tree1");
+
+// Storage setters & getters
 export function setter_demo_str(value: string): void {
   storage.set<string>("var-string", value)
 }
@@ -12,22 +19,11 @@ export function setter_demo_u128(value: u128): void {
   storage.set<u128>("var-u128", value)
 }
 
-// Storage getters
 export function getter_demo_str(): string {
   return storage.getPrimitive<string>("var-string", "default value")
 }
 
 export function getter_demo_u128(): u128 {
-  if (storage.contains('var-u128')) {
-    return storage.getSome<u128>("var-u128")
-  }
-  return ONE_NEAR
+  if (!storage.contains('var-u128')) { return ONE_NEAR }
+  return storage.getSome<u128>("var-u128")
 }
-
-// Collections
-import { PersistentMap, PersistentVector, PersistentSet, AVLTree } from "near-sdk-as";
-
-export let vector = new PersistentVector<i32>("unique-id-vector1");
-export let map = new PersistentMap<string, i32>("unique-id-map1");
-export let set = new PersistentSet<i32>("unique-id-set1");
-export let tree = new AVLTree<string, i32>("unique-id-tree1");
