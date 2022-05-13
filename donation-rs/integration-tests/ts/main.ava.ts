242ea2cce1d8c23c63ac58a734d0635583f68794
gagdiez <gagdiez.c@gmail.com>
2022-05-13 18:31:49 +0200

    Changed format for docs

--- donation-rs/integration-tests/ts/main.ava.ts
+++ donation-rs/integration-tests/ts/main.ava.ts
@@ -6,7 +6,7 @@ const test = anyTest as TestFn<{
   accounts: Record<string, NearAccount>;
 }>;
 
-test.before(async (t) => {
+test.beforeEach(async (t) => {
   // Init the worker and start a Sandbox server
   const worker = await Worker.init();
 
@@ -38,17 +38,10 @@ test.before(async (t) => {
   t.context.accounts = { root, contract, beneficiary, alice, bob };
 });
 
-test.afterEach(async (t) => {
-  // Stop Sandbox server
-  await t.context.worker.tearDown().catch((error) => {
-    console.log("Failed to stop the Sandbox:", error);
-  });
-});
-
-/* test("cannot be initialized twice", async (t) => {
+test("cannot be initialized twice", async (t) => {
   const { contract, alice, beneficiary } = t.context.accounts;
   await t.throwsAsync(alice.call(contract, "init", { beneficiary: beneficiary.accountId }));
-}); */
+});
 
 test("sends donations to the beneficiary", async (t) => {
   const { contract, alice, bob, beneficiary } = t.context.accounts;
@@ -75,13 +68,16 @@ test("records the donation", async (t) => {
 
   t.is(donation_idx, 3)
 
+  class Donation{ donor: string = ""; amount: Number = 0; }
   const donation: Donation = await contract.view("get_donation_by_number", {donation_number: donation_idx})
   
   t.is(donation.donor, alice.accountId)
   t.is(donation.amount, 3e+24)
 });
 
-class Donation{
-  donor: string = "";
-  amount: Number = 0;
-}
+test.afterEach(async (t) => {
+  // Stop Sandbox server
+  await t.context.worker.tearDown().catch((error) => {
+    console.log("Failed to stop the Sandbox:", error);
+  });
+});
