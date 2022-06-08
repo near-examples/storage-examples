7e2cc12e1354fa7ea3205a60651464e602b99190
gagdiez <gagdiez.c@gmail.com>
2022-06-08 16:18:25 +0200

    added a frontend

---
+++ donation-as/frontend/assets/js/near/config.js
@@ -0,0 +1,35 @@
+import { keyStores } from 'near-api-js'
+
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'new-awesome-project'
+
+function getConfig(env) {
+  switch (env) {
+
+  case 'production':
+  case 'mainnet':
+    return {
+      networkId: 'mainnet',
+      nodeUrl: 'https://rpc.mainnet.near.org',
+      contractName: CONTRACT_NAME,
+      walletUrl: 'https://wallet.near.org',
+      helperUrl: 'https://helper.mainnet.near.org',
+      explorerUrl: 'https://explorer.mainnet.near.org',
+      deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() }
+    }
+  case 'development':
+  case 'testnet':
+    return {
+      networkId: 'testnet',
+      nodeUrl: 'https://rpc.testnet.near.org',
+      contractName: CONTRACT_NAME,
+      walletUrl: 'https://wallet.testnet.near.org',
+      helperUrl: 'https://helper.testnet.near.org',
+      explorerUrl: 'https://explorer.testnet.near.org',
+      deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() }
+    }
+  default:
+    throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`)
+  }
+}
+
+module.exports = getConfig
