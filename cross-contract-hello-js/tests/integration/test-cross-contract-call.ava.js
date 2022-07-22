d7ae8e69ea76268341313ea6193356529c59bb48
Dennis <10233439+idea404@users.noreply.github.com>
2022-07-22 13:35:53 +0000

    add ava req missing assertion

--- cross-contract-hello-js/tests/integration/test-cross-contract-call.ava.js
+++ cross-contract-hello-js/tests/integration/test-cross-contract-call.ava.js
@@ -1,6 +1,5 @@
 import { Worker } from "near-workspaces";
 import test from "ava";
-import path from "path";
 
 test.beforeEach(async (t) => {
     // Init the worker and start a Sandbox server
@@ -10,14 +9,13 @@ test.beforeEach(async (t) => {
     const root = worker.rootAccount;
 
     // Deploy the onCall contract.
-    const xcc = await root.createAndDeploy(root.getSubAccount("xcc").accountId, path.resolve("./build/xcc.wasm"));
+    const xcc = await root.createAndDeploy(root.getSubAccount("xcc").accountId, "./build/xcc.wasm");
 
     // Deploy status-message the contract.
-    const helloNear = await root.createAndDeploy(root.getSubAccount("hello-near").accountId, path.resolve("./build/hello-near.wasm"));
+    const helloNear = await root.createAndDeploy(root.getSubAccount("hello-near").accountId, "./build/hello-near.wasm");
 
-    // Create test accounts
+    // Create test account alice
     const alice = await root.createSubAccount("alice");
-    const bob = await root.createSubAccount("bob");
 
     // Save state for test runs, it is unique for each test
     t.context.worker = worker;
@@ -26,7 +24,6 @@ test.beforeEach(async (t) => {
         helloNear,
         xcc,
         alice,
-        bob,
     };
 });
 
@@ -39,21 +36,22 @@ test.afterEach(async (t) => {
 test("Initializes", async (t) => {
     const { xcc } = t.context.accounts;
     await xcc.call(xcc, "init", {});
+    t.pass();
 });
 
 test("returns the default greeting", async (t) => {
-    const { xcc } = t.context.accounts;
+    const { xcc, alice } = t.context.accounts;
     await xcc.call(xcc, "init", {});
-    const message = await xcc.call(xcc, "query_greeting", {}, { gas: 200000000000000 });
+    const message = await alice.call(xcc, "query_greeting", {}, { gas: 200000000000000 });
     t.is(message, "Hello");
 });
 
 test("change the greeting", async (t) => {
-    const { xcc } = t.context.accounts;
+    const { xcc, alice } = t.context.accounts;
     await xcc.call(xcc, "init", {});
-    const hello = await xcc.call(xcc, "query_greeting", {}, { gas: 200000000000000 });
+    const hello = await alice.call(xcc, "query_greeting", {}, { gas: 200000000000000 });
     t.is(hello, "Hello");
-    await xcc.call(xcc, "change_greeting", { new_greeting: "Howdy" }, { gas: 200000000000000 });
-    const howdy = await xcc.call(xcc, "query_greeting", {}, { gas: 200000000000000 });
+    await alice.call(xcc, "change_greeting", { new_greeting: "Howdy" }, { gas: 200000000000000 });
+    const howdy = await alice.call(xcc, "query_greeting", {}, { gas: 200000000000000 });
     t.is(howdy, "Howdy");
 });
