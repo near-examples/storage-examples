3e5086181e90c5a328e8c70dfd3fe1c62a806a97
Rupert <gagdiez@MacBook-Pro.home>
2022-08-01 16:33:44 +0200

    updated xcc-rs example

--- cross-contract-hello-rs/contract/src/external.rs
+++ cross-contract-hello-rs/contract/src/external.rs
@@ -1,4 +1,4 @@
-use near_sdk::{env, ext_contract, log, PromiseResult};
+use near_sdk::{ext_contract};
 
 pub const TGAS: u64 = 1_000_000_000_000;
 pub const NO_DEPOSIT: u128 = 0;
@@ -17,17 +17,4 @@ trait HelloNear {
   #[payable]
   fn get_greeting(&mut self) -> String;
   fn set_greeting(&self, message: String);
-}
-
-// Aux functions to interact with the validator
-pub fn did_promise_succeed() -> bool {
-  if env::promise_results_count() != 1 {
-    log!("Expected a result on the callback");
-    return false;
-  }
-
-  match env::promise_result(0) {
-    PromiseResult::Successful(_) => true,
-    _ => false,
-  }
 }
