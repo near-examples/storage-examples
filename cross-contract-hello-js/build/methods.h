0d7016cea4fc012119bcffe1c25f6c06b7e10d92
gagdiez <gagdiez.c@gmail.com>
2022-08-05 15:07:12 +0200

    removed unnecesary build folder

--- cross-contract-hello-js/build/methods.h
+++
@@ -1,5 +0,0 @@
-DEFINE_NEAR_METHOD(change_greeting)
-DEFINE_NEAR_METHOD(change_greeting_callback)
-DEFINE_NEAR_METHOD(init)
-DEFINE_NEAR_METHOD(query_greeting)
-DEFINE_NEAR_METHOD(query_greeting_callback)
