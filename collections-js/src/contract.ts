e6f606b60dd2cbf9e0af4988896184221a9c40d0
garikbesson <garikbesson@gmail.com>
2024-07-25 22:00:40 +0200

    methods and tests for js contract

--- collections-js/src/contract.ts
+++ collections-js/src/contract.ts
@@ -1,134 +1,126 @@
 // Find all our documentation at https://docs.near.org
 import { NearBindgen, call, view, near, LookupSet, UnorderedSet, UnorderedMap, Vector, initialize, LookupMap } from "near-sdk-js";
-import { assert } from "near-sdk-js";
 
 @NearBindgen({})
 class Storage {
+  static schema = {
+    vector: Vector,
+    lookup_set: LookupSet,
+    unordered_set: UnorderedSet,
+    lookup_map: LookupMap,
+    unordered_map: UnorderedMap,
+    outerMap: { class: UnorderedMap, value: UnorderedMap }
+  };
+  
   vector: Vector<number> = new Vector<number>('unique-id-vector1');
   lookup_set: LookupSet<number> = new LookupSet<number>('unique-id-set1');
   unordered_set: UnorderedSet<number> = new UnorderedSet<number>('unique-id-set2');
   lookup_map: LookupMap<number> = new LookupMap<number>('unique-id-map1');
   unordered_map: UnorderedMap<number> = new UnorderedMap<number>('unique-id-map2');
-  demo_string: String = "";
-
-  @initialize({})
-  init({demo_string}: {demo_string: string}) {
-    this.demo_string = demo_string;
-  }
+  outerMap: UnorderedMap<UnorderedMap<string>> = new UnorderedMap<UnorderedMap<string>>('unique-id-nested1');
 
   // Vector
   @call({})
-  vector_push({value}:{value:number}) {
+  push_vector({ value }: { value:number }) {
     this.vector.push(value);
   }
 
   @view({})
-  vector_get(index: number): number {
+  get_vector({ index }: { index: number }): number {
     return this.vector.get(index);
   }
 
   @call({})
-  vector_replace({index, value}:{index: number, value:number}) {
+  replace_vector({ index, value }: { index: number, value:number }) {
     this.vector.replace(index, value);
   }
 
   @view({})
-  vector_len(index: number): number {
+  len_vector(): number {
     return this.vector.length;
   }
 
-  // @view({})
-  // vector_iter({from_index, limit}:{from_index: number, limit:number}) {
-  //   this.avector.
-  // }
+  @view({})
+  iter_vector({ from_index, limit }: { from_index: number, limit: number }) {
+    return this.vector.toArray().slice(from_index, limit);
+  }
 
   // LookupSet
   @call({})
-  insert_set({value}:{value:number}) {
+  insert_lookup_set({ value }: { value: number }) {
     this.lookup_set.set(value);
   }
 
   @call({})
-  remove_set({value}:{value:number}) {
+  remove_lookup_set({ value }: { value: number }) {
     this.lookup_set.remove(value);
   }
 
   @view({})
-  contains_set(value: number): boolean {
+  contains_lookup_set({ value }: { value: number }): boolean {
     return this.lookup_set.contains(value);
   }
 
   // UnorderedSet
   @call({})
-  insert_unordered_set({value}:{value:number}) {
+  insert_unordered_set({ value }: { value: number }) {
     this.unordered_set.set(value);
   }
 
   @call({})
-  remove_unordered_set({value}:{value:number}) {
+  remove_unordered_set({ value }: { value: number }) {
     this.unordered_set.remove(value);
   }
 
   @view({})
-  contains_unordered_set(value: number): boolean {
+  contains_unordered_set({ value }: { value: number }): boolean {
     return this.unordered_set.contains(value);
   }
 
-  // @view({})
-  // iter_unordered_set({from_index, limit}:{from_index: number, limit:number}) {
-  //   this.unordered_set.
-  // }
+  @view({})
+  iter_unordered_set({ from_index, limit }: { from_index: number, limit:number }) {
+    return this.unordered_set.toArray().slice(from_index, limit);
+  }
 
   // LookupMap
   @call({})
-  insert_map({key, value}:{key: string, value:number}) {
+  insert_lookup_map({ key, value }: { key: string, value: number }) {
     this.lookup_map.set(key, value);
   }
 
   @call({})
-  remove_map({key}:{key: string}) {
+  remove_lookup_map({ key }: { key: string }) {
     this.lookup_map.remove(key);
   }
 
   @view({})
-  get_map(key: string): number {
+  get_lookup_map({ key }: { key: string }): number {
     return this.lookup_map.get(key);
   }
 
   @view({})
-  contains_key_map(key: string): boolean {
+  contains_key_lookup_map({ key }: { key: string }): boolean {
     return this.lookup_map.containsKey(key);
   }
 
   // UnorderedMap
   @call({})
-  insert_unordered_map({key, value}:{key: string, value:number}) {
+  insert_unordered_map({ key, value }: { key: string, value: number }) {
     this.unordered_map.set(key, value);
   }
 
   @call({})
-  remove_unordered_map({key}:{key: string}) {
+  remove_unordered_map({ key }: { key: string }) {
     this.unordered_map.remove(key);
   }
 
   @view({})
-  get_unordered_map(key: string): number {
+  get_unordered_map({ key }: { key: string }): number {
     return this.unordered_map.get(key);
   }
 
   @view({})
-  contains_key_unordered_map(key: string): boolean {
-    return this.unordered_map.(key);
-  }
-
-  @view({}) // This method is read-only and can be called for free
-  get_greeting(): String {
-    return this.demo_string;
-  }
-
-  @call({}) // This method changes the state, for which it cost gas
-  set_greeting({ greeting }: { greeting: string }): void {
-    near.log(`Saving greeting ${greeting}`);
-    this.demo_string = greeting;
+  iter_unordered_map({ from_index, limit }: { from_index: number, limit:number }) {
+    return this.unordered_map.toArray().slice(from_index, limit);
   }
 }
